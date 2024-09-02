3841b3334a0227341eb483603ec91776d2063cd7
Nahuel <nahuelcarlos150@gmail.com>
2024-09-02 16:39:18 -0300

    se hizo el hash de contraseñas

--- backend-sessions/controllers/main.controllers.js
+++ backend-sessions/controllers/main.controllers.js
@@ -1,10 +1,16 @@
 import { ConnectionDataBase } from "../src/database.js";
+import bcrypt from 'bcrypt';
 
 const registrarUsuarios = async (req,res) => {
     const {username, password} = req.body
-    const connection = await ConnectionDataBase()
-    await connection.query("INSERT INTO `users`(`username`, `password`) VALUES (?,?)",[username,password])
-    connection.end()
+    try {
+        const hashPassword = await bcrypt.hash(password,10)
+        const connection = await ConnectionDataBase()
+        await connection.query("INSERT INTO `users`(`username`, `password`) VALUES (?,?)",[username,hashPassword])
+        connection.end()
+    } catch (error) {
+        console.error("NO SE PUDO REGISTRAR",error)
+    }
 } 
 
 export {registrarUsuarios}
